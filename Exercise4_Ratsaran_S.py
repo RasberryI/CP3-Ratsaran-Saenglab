fa0daed449a7685616ab27154264c7d0618cb427
RasberryI <ratsaransaenglab@gmail.com>
2024-04-29 14:17:17 +0700

    Create Exercise4_Ratsaran_S.py

---
+++ Exercise4_Ratsaran_S.py
@@ -0,0 +1,9 @@
+Fe = 85
+Gb = 90
+Ic = 95
+Cp = 100
+print("--- Your Score ---")
+print("Foundation English :", Fe)
+print("General Business :", Gb)
+print("Introduction to Computer Systems :", Ic)
+print("Computer Programming :", Cp)
