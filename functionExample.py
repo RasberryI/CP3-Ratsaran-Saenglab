b81be7bcf26f6fca3ded8106a46fef76a6bfcb23
RasberryI <ratsaransaenglab@gmail.com>
2024-05-21 10:58:24 +0700

    Create functionExample.py

---
+++ functionExample.py
@@ -0,0 +1,16 @@
+def addnumber():
+    x = int(input())
+    y = int(input())
+    print(x+y)
+def dividenumber():
+    x = int(input())
+    y = int(input())
+    print(x/y)
+def multiplenum():
+    x = int(input())
+    y = int(input())
+    print(x*y)
+def minusnumber():
+    x = int(input())
+    y = int(input())
+    print(x-y)
